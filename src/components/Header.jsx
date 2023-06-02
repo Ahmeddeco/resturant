9824412efb023389c28ba94178120cfaa4c32e97
ahmed <hetetlahma@gmail.com>
2023-06-02 04:09:31 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,38 +61,70 @@ const Header = () => {
       animate={isActive ? "show" : ""}
       className='bg-pink-200/20 fixed w-full max-w-[1800px] z-50 py-4'
     >
-      <div className='container mx-auto'>
-        {/* menu button */}
-        <motion.div onClick={() => setNav(!nav)} className='cursor-pointer flex '>
-          menu button
-        </motion.div>
-        {/* Logo */}
-        <div className=''>
-          <a href='/' className=''>
-            {/* if header is active make logo 90 x 90 px else 107 x 107 px */}
-            <img
-              className={`${
-                isActive ? "w-[90px] h-[90px]" : "w-[107px] h-[107px]"
-              }`}
-              src={LogoWhite}
-              alt='logo'
-            />
-          </a>
+      <motion.div
+        variants={staggerContainer(0.3, 1)}
+        initial='hidden'
+        animate={"show"}
+        className='container mx-auto'
+      >
+        <div className='flex justify-between items-center px-4 lg:px-0 relative text-white'>
+          {/* menu button */}
+          <motion.div
+            className={`${
+              nav ? "gap-y-0" : "gap-y-2"
+            } cursor-pointer flex flex-col items-center justify-center w-12 h-12 p-3 order-2 lg:order-none border-2 rounded-full`}
+            onClick={() => setNav(!nav)}
+          >
+            {/* bar1 */}
+            <motion.div
+              initial={{
+                rotate: 0,
+              }}
+              animate={{
+                rotate: nav ? -45 : 0,
+                translateY: nav ? 2 : 0,
+              }}
+              className='w-full h-[2px] bg-white'
+            ></motion.div>
+            {/* bar2 */}
+            <motion.div
+              initial={{
+                rotate: 0,
+              }}
+              animate={{
+                rotate: nav ? 45 : 0,
+              }}
+              className='w-full h-[2px] bg-white'
+            ></motion.div>
+          </motion.div>
+          {/* Logo */}
+          <motion.div variants={fadeIn("down", "tween", 1.2, 1.4)} className=''>
+            <a href='/' className=''>
+              {/* if header is active make logo 90 x 90 px else 107 x 107 px */}
+              <img
+                className={`${
+                  isActive ? "w-[90px] h-[90px]" : "w-[107px] h-[107px]"
+                }`}
+                src={LogoWhite}
+                alt='logo'
+              />
+            </a>
+          </motion.div>
+          {/* Social icon */}
+          <div className='hidden lg:flex'>
+            <Socials />
+          </div>
+          {/* Nav */}
+          <motion.div
+            variants={navVariants}
+            initial='hidden'
+            animate={nav ? "show" : ""}
+            className='absolute bg-accent w-[310px] h-[50vh] right-0 lg:left-0 top-[120px] bottom-0 z-50 rounded-lg shadow-xl'
+          >
+            <Nav />
+          </motion.div>
         </div>
-        {/* Social icon */}
-        <div className=''>
-          <Socials />
-        </div>
-        {/* Nav */}
-        <motion.div
-          variants={navVariants}
-          initial='hidden'
-          animate={nav ? "show" : ""}
-          className='absolute bg-accent w-[310px] h-[50vh] right-0 lg:left-0 top-[120px] bottom-0 z-50 rounded-lg shadow-xl'
-        >
-          <Nav />
-        </motion.div>
-      </div>
+      </motion.div>
     </motion.header>
   );
 };
